212a5fd8525d414a24f5a6d3e31dea3af0e5055b
Indicestudio <indicestudio@gmail.com>
2017-01-17 20:37:17 -0300

    servicios & clientes roulette

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  var $clientcarousel = $('#clients-list');
+  var clients = $clientcarousel.children().length;
+  var clientwidth = (clients * 320); // 140px width for each client item
+  $clientcarousel.css('width', clientwidth);
+
+  var rotating = true;
+  var clientspeed = 0;
+  var seeclients = setInterval(rotateClients, clientspeed);
+
+  $(document).on({
+    mouseenter: function() {
+      rotating = true; // turn off rotation when hovering
+    },
+    mouseleave: function() {
+      rotating = true;
+    }
+  }, '#clients');
+
+  function rotateClients() {
+    if (rotating != false) {
+      var $first = $('#clients-list li:first');
+      $first.animate({
+        'margin-left': '-320px'
+      }, 3500, 'linear', function() {
+        $first.remove().css({
+          'margin-left': '0px'
+        });
+        $('#clients-list li:last').after($first);
+      });
+    }
+  }
+});
